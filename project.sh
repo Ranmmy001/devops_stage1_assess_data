8c7e9f1c65c6de370cbabdeae0c18b2ce48a8f5c
Ranmmy001 <olaranmmyola@gmail.com>
2025-10-30 16:18:59 +0100

    my new update

--- project.sh
+++ project.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 
-read -p "Enter your name: " name
+read -p "Enter your Firstname: " Firstname
 
-echo "Welcome, $name!"
+read -p "Enter your Lastname: " Lastname
+
+read -p "Enter Email address: " Email_address
+
+read -p "Enter your password: " Password
+
+user=("$Firstname" "$Lastname" "$Email_address" "$Password")
+
+echo "${user[@]}" 
+
+echo "Welcome, ${user[@]} !"
